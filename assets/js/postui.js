bfa924cfdcd9b363144b5140f49e41502d5b1031
Brian Ressler <carlofabyss@hotmail.com>
2023-07-17 14:14:21 -0500

    Attempt to resize expandable button to match table width on hidden get involved page

--- assets/js/postui.js
+++ assets/js/postui.js
@@ -1,4 +1,84 @@
 $(document).ready(function(){
-  console.log("Injecting post specific content...");
+  console.log("TOPS: Injecting post specific UI...");
+  
+  function inject_expandable_tables() {
+	  let ex_tables = document.getElementsByClassName("tops_expandable_table");
+	  
+	  if(ex_tables != null) {
+		  if(ex_tables.length > 0) {
+			  console.log("TOPS: Found " + ex_tables.length + " tables");
+			  
+			  for(i = 0; i < ex_tables.length; i++) {
+				  let current_ex_table = ex_tables[i];
+				  let the_table = current_ex_table.getElementsByTagName("table");
+				  let the_table_obj;
+				  let the_table_width = 0;
+				  
+				  let the_table_opt = current_ex_table.getElementsByClassName("tops_expandable_opt");
+				  let the_table_opt_obj;
+				  
+				  
+				  if(the_table != null) {
+					  if(the_table.length == 1) {
+						  the_table_obj = the_table[0];
+						  let table_positionInfo = the_table_obj.getBoundingClientRect();
+						  the_table_width = table_positionInfo.width;
+					  }
+				  }
+				  
+				  if(the_table_opt != null) {
+					  if(the_table_opt.length == 1) {
+						  the_table_opt_obj = the_table_opt[0];
+						  the_table_opt_obj.style = "width: " + the_table_width + "px;";
+					  }
+				  }
+				  
+			  }
+			  
+		  }
+	  }
+	  
+  }
+  
+  inject_expandable_tables();
+  
+  window.addEventListener('resize', function(event) {
+	let ex_tables = document.getElementsByClassName("tops_expandable_table");
+	  
+	  if(ex_tables != null) {
+		  if(ex_tables.length > 0) {
+			  console.log("TOPS: Found " + ex_tables.length + " tables");
+			  
+			  for(i = 0; i < ex_tables.length; i++) {
+				  let current_ex_table = ex_tables[i];
+				  let the_table = current_ex_table.getElementsByTagName("table");
+				  let the_table_obj;
+				  let the_table_width = 0;
+				  
+				  let the_table_opt = current_ex_table.getElementsByClassName("tops_expandable_opt");
+				  let the_table_opt_obj;
+				  
+				  
+				  if(the_table != null) {
+					  if(the_table.length == 1) {
+						  the_table_obj = the_table[0];
+						  let table_positionInfo = the_table_obj.getBoundingClientRect();
+						  the_table_width = table_positionInfo.width;
+					  }
+				  }
+				  
+				  if(the_table_opt != null) {
+					  if(the_table_opt.length == 1) {
+						  the_table_opt_obj = the_table_opt[0];
+						  the_table_opt_obj.style = "width: " + the_table_width + "px;";
+					  }
+				  }
+				  
+			  }
+			  
+		  }
+	  }
+  }, true);
+  
   
 });
